639bee3b912629cce47e62baea5c0b9e95b18923
Leon <leonivanrodrigo@gmail.com>
2019-05-23 15:39:22 -0300

    Fin maquetado.

--- app.js
+++ app.js
@@ -1,7 +1,61 @@
 const mazo = document.querySelectorAll('.carta');
+let hayCartaVolteada = bloquearTablero = false;
+let primeraCarta, segundaCarta;
 
-function rotarCarta () {
-    this.classList.toggle('rotar');
-}
 mazo.forEach(carta => carta.addEventListener('click', rotarCarta));
+
+mazo.forEach(carta => {
+    const posicionAleatoria = Math.floor(Math.random() * mazo.length)
+    carta.style.order = posicionAleatoria;
+});
+
+function rotarCarta() {
+    if (bloquearTablero) {
+        return;
+    }
+    if (this === primeraCarta) {
+        return;
+    }
+
+    this.classList.add('rotar');
+
+    if (!hayCartaVolteada) {
+        hayCartaVolteada = true;
+        primeraCarta = this;
+        return;
+    }
+    segundaCarta = this;
+    hayCartaVolteada = false;
+
+    chequearCoincidencia();
+}
+
+function chequearCoincidencia() {
+    let coinciden = primeraCarta.getAttribute('data-framework') === segundaCarta.getAttribute('data-framework');
+    coinciden ? desactivarCartas() : resetearCartas();
+}
+
+function desactivarCartas() {
+    primeraCarta.addEventListener('click', rotarCarta);
+    segundaCarta.addEventListener('click', rotarCarta);
+
+    resetearTablero();
+}
+// Vuelven a estado originial si no encuentran coincidencia.
+function resetearCartas() {
+    bloquearTablero = true;
+
+    setTimeout(() => {
+        primeraCarta.classList.remove('rotar');
+        segundaCarta.classList.remove('rotar');
+
+        resetearTablero();
+    }, 1000);
+
+}
+
+function resetearTablero() {
+    hayCartaVolteada = bloquearTablero = false;
+    primeraCarta = segundaCarta = false;
+}
 
